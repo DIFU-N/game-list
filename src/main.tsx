4ea3059c9c94733a6c09b09069ec5282785ac719
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-19 12:26:57 +0100

    no .tsx in import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// remove the .tsx from the App.tsx
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
