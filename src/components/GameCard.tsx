57d73bbd23677a42d772b8e954a6ad0c46fdbf10
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-20 13:11:49 +0100

    GameCard begins

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,22 @@
+import { Game } from "../types";
+import { NavLink } from "react-router-dom";
+
+interface Props {
+  content: Game;
+}
+
+const GameCard = ({ content }: Props): React.ReactElement => {
+  const { id, title, thumbnail, short_description, genre } = content;
+
+  const link = `game/${id}`;
+  return (
+    <NavLink to={link}>
+      <img src={thumbnail} alt={`${title} logo`} />
+      <h2>{title}</h2>
+      <p>{short_description}</p>
+      <p>{genre}</p>
+    </NavLink>
+  );
+};
+
+export default GameCard;
