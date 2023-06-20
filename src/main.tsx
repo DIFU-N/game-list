0a2b12e232e8482390265436926fb9025d941cfd
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-20 17:22:48 +0100

    store provided

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 // remove the .tsx from the App.tsx
 import App from './App'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
