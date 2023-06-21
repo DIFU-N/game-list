e27ae76c9f455cb4454573f4a7a5af7c1be3d342
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-21 09:59:03 +0100

    App.jsx wrapped by error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './app/store'
+import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </Provider>
   </React.StrictMode>,
 )
