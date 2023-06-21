88c62f215132d486eb1b0a123af616329a3ab6be
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-21 12:50:09 +0100

    theme.ts init

---
+++ src/style/theme.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  colors: {
+    text: "#333",
+    background: "#fff",
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    text: "#fff",
+    background: "#333",
+  },
+};
