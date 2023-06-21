92ec2b90eeae79ea7f9bcbc591bfb8a2b44eec7a
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-21 12:58:49 +0100

    themeProvider begins

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 // remove the .tsx from the App.tsx
 import App from './App'
@@ -6,12 +5,16 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './app/store'
 import ErrorBoundary from './components/ErrorBoundary'
+import ThemeProvider from './components/ThemeProvider'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <ErrorBoundary>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </ErrorBoundary>
     </Provider>
   </React.StrictMode>,
